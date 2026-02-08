e909b9869568689a3dbad9ff29dc6d3564551eca
SungGun Lee <66353492+henryz0113@users.noreply.github.com>
2026-02-08 14:39:06 +0900

    Create main.tf

---
+++ pipeline/main.tf
@@ -0,0 +1,15 @@
+# Create a random name for the resource group using random_pet
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+# Create a resource group using the generated random name
+resource "azurerm_resource_group" "example" {
+  location = var.resource_group_location
+#  name     = random_pet.rg_name.id
+   name     = "my-first-rg"
+}
+# Second resource group
+resource "azurerm_resource_group" "example2" {
+  location = var.resource_group_location
+  name     = "my-second-rg"
+}
