a1657bc2c13e019bb5b07c8e17da97cbf1689067
SungGun Lee <66353492+henryz0113@users.noreply.github.com>
2026-02-08 14:52:47 +0900

    Add files via upload

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -1,15 +1,6 @@
-# Create a random name for the resource group using random_pet
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-# Create a resource group using the generated random name
-resource "azurerm_resource_group" "example" {
-  location = var.resource_group_location
-#  name     = random_pet.rg_name.id
-   name     = "my-first-rg"
-}
-# Second resource group
-resource "azurerm_resource_group" "example2" {
-  location = var.resource_group_location
-  name     = "my-second-rg"
+resource "random_uuid" "uuid" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-hello-tf-${random_uuid.uuid.result}"
+  location = var.location
 }
